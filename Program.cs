f2a7ae56b53b734439a10f6f4ad41cf16da693f2
Salvador <sarevalotamayo@student.rtc.edu>
2024-02-25 18:50:09 -0800

    Update code

--- Program.cs
+++ Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter a year:");
+            int year = Convert.ToInt32(Console.ReadLine());
+
+            bool isLeapYear = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+
+            if (isLeapYear)
+            {
+                Console.WriteLine(year + " is a leap year.");
+            }
+            else
+            {
+                Console.WriteLine(year + " is not a leap year.");
+            }
         }
     }
 }
